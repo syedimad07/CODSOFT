c94c5ab95cd40442e80471e46dbf167c09052cc1
Syed Imaduddin <informsyedimad@gmail.com>
2023-11-26 19:41:10 +0530

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+    fontFamily : {
+      signature : ["Great Vibes"],
+      tinos : ["Tinos"]
+    }        
+  },
+  plugins: [],
+}
+
