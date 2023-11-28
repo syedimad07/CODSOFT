eb0ce09087facf5951c83c14f5589428c8b71ce8
Syed Imaduddin <informsyedimad@gmail.com>
2023-11-29 02:34:41 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,16 @@ module.exports = {
     fontFamily : {
       signature : ["Great Vibes"],
       tinos : ["Tinos"],
+      tinosD : ["Tinos"]
     },
     
     colors: {
       'dark-grey': "#043944",
       'Beaut-P': "#bf4aa8",
       'white-d': "#d9d9d9",
-      'tan-d': "#c9af98"
+      'tan-d': "#c9af98",
+      'blue-greeny': "#5cbdb9",
+      'black': "#000000",
     }
            
   },
